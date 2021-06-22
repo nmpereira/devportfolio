c3fd629c15c378c7b5f3ce87f73145f1fcbe19df
Nikhil Mario Pereira <45009203+nmpereira@users.noreply.github.com>
2021-06-22 00:07:03 -0400

    Pdfjs text layer (#3)
    * text-layer-works
    
    * Reworked pdfjs to add text layer
    
    * Replace pdfjs node module with cdn
    
    * CSS updates to pdf page

--- cv.js
+++ cv.js
@@ -1,16 +1,68 @@
-// If absolute URL from the remote server is provided, configure the CORS
+ "use strict";
+
+
+  
+    if (!pdfjsLib.getDocument || !pdfjsViewer.PDFPageView) {
+      // eslint-disable-next-line no-alert
+      alert("Please build the pdfjs-dist library using\n  `gulp dist-install`");
+    }
+  
+    // The workerSrc property shall be specified.
+    //
+    pdfjsLib.GlobalWorkerOptions.workerSrc =
+      "https://cdn.jsdelivr.net/npm/pdfjs-dist@2.9.359/build/pdf.worker.js";
+  
+    // Some PDFs need external cmaps.
+    const CMAP_URL = "https://cdn.jsdelivr.net/npm/pdfjs-dist@2.9.359/cmaps/";
+    const CMAP_PACKED = true;
+  
+    const DEFAULT_URL = "../nmpereira_cv.pdf";
+    const PAGE_TO_VIEW = 1;
+    const SCALE = 0.835;
+  
+    const container = document.getElementById("pageContainer");
+  
+    const eventBus = new pdfjsViewer.EventBus();
+  
+    // Loading document.
+    const loadingTask = pdfjsLib.getDocument({
+      url: DEFAULT_URL,
+      cMapUrl: CMAP_URL,
+      cMapPacked: CMAP_PACKED,
+    });
+    loadingTask.promise.then(function (pdfDocument) {
+      // Document loaded, retrieving the page.
+      return pdfDocument.getPage(PAGE_TO_VIEW).then(function (pdfPage) {
+        // Creating the page view with default parameters.
+        const pdfPageView = new pdfjsViewer.PDFPageView({
+          container,
+          id: PAGE_TO_VIEW,
+          scale: SCALE,
+          defaultViewport: pdfPage.getViewport({ scale: SCALE }),
+          eventBus,
+          // We can enable text/annotations layers, if needed
+          textLayerFactory: new pdfjsViewer.DefaultTextLayerFactory(),
+          annotationLayerFactory: new pdfjsViewer.DefaultAnnotationLayerFactory(),
+        });
+        // Associates the actual page with the view, and drawing it
+        pdfPageView.setPdfPage(pdfPage);
+        return pdfPageView.draw();
+      });
+    });
+
+    // If absolute URL from the remote server is provided, configure the CORS
 // header on that server.
-var url = 'https://nmpereira.com/nmpereira_cv.pdf';
+var url = DEFAULT_URL;
 
 // Loaded via <script> tag, create shortcut to access PDF.js exports.
 var pdfjsLib = window['pdfjs-dist/build/pdf'];
 
 // The workerSrc property shall be specified.
-pdfjsLib.GlobalWorkerOptions.workerSrc = '//mozilla.github.io/pdf.js/build/pdf.worker.js';
+//pdfjsLib.GlobalWorkerOptions.workerSrc = 'https://cdn.jsdelivr.net/npm/pdfjs-dist@2.9.359/build/pdf.worker.js';
 
 // Asynchronous download of PDF
-var loadingTask = pdfjsLib.getDocument(url);
-loadingTask.promise.then(function(pdf) {
+var loadingTaskmobile = pdfjsLib.getDocument(url);
+loadingTaskmobile.promise.then(function(pdf) {
   console.log('PDF loaded');
 
   // Fetch the first page
